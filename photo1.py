d3cad1c600473e84b132bd58eb614b814556cd80
dipakrasal2009 <dipakrasal2009@gmail.com>
2024-07-09 11:34:23 +0530

    update the mane of image window

--- photo1.py
+++ photo1.py
@@ -2,7 +2,7 @@ import cv2
 cap = cv2.VideoCapture(0)
 status,photo = cap.read()
 print(status)
-cv2.imshow("Dipak Rasal",photo)
+cv2.imshow("Image Window",photo)
 cv2.imwrite("Photo.jpg",photo)
 cv2.waitKey()
 cv2.destroyAllWindows()
