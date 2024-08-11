47ccc694201eee217a6ab95c4483099616f8282f
dipakrasal2009 <dipakrasal2009@gmail.com>
2024-08-11 10:20:31 +0530

    🚀 Automation in Action: Launching AWS EC2 Instances with Boto3! 🚀
    Excited to share that I’ve successfully launched AWS EC2 instances using the Boto3 library in Python! This project has been a fantastic learning experience, enabling me to automate the process of deploying multiple instances within seconds. 📚✨
    
    🌟 Project Highlights:
    
        Boto3 Library: Leveraged Python’s Boto3 library to interact with AWS services programmatically, making it easier to manage cloud resources.
        AWS EC2: Automated the launch of multiple EC2 instances, drastically reducing the time required to deploy cloud infrastructure.
        Efficiency: The code allows for the rapid deployment of multiple operating systems with just a few lines of code.
    
    🔗 Check out the GitHub repository here: [Your GitHub Link]
    
    This project is another step in my journey to mastering cloud automation and infrastructure as code. Special thanks to Mr. Vimal Daga and Linux World Informatics Pvt. Ltd. for the continuous guidance and support.
    
    Looking forward to applying these skills in real-world scenarios and collaborating with other tech enthusiasts! 🚀💻
    
    #AWS #EC2 #Boto3 #Python #CloudAutomation #InfrastructureAsCode #GitHub #LearningJourney #LinuxWorldInformatics #VimalDaga

--- Launch_Instance_using_Boto3.py
+++ Launch_Instance_using_Boto3.py
@@ -3,8 +3,8 @@ import boto3
 
 myec2 = boto3.resource(service_name = "ec2",
                         region_name = 'ap-south-1',
-                        aws_access_key_id = "AKIAVRUVRDUTWFV5DG7Q",
-                        aws_secret_access_key = "Grl7M+UXQu3FLHcs8AoTvKlAbKCtFrXM/z5MO+aD"
+                        aws_access_key_id = "add to your access key",
+                        aws_secret_access_key = "add to your secret access key"
                         )
 
 def Launch_Instance():
